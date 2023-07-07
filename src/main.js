1601d137173cb5281d9ea0f449be4306dbe8df22
jay.kou <jay.kou@qq.com>
2023-07-07 16:06:41 +0800

    feat: 用prism来处理高亮

--- src/main.js
+++ src/main.js
@@ -10,11 +10,12 @@ import { toHast } from "mdast-util-to-hast";
 import { visit } from "unist-util-visit";
 import { remove } from "unist-util-remove";
 import { sanitize, defaultSchema } from "hast-util-sanitize";
-import { zwitch } from "zwitch";
 import { parseFragment } from "parse5";
 import { fromParse5 } from "hast-util-from-parse5";
 
-import hljs from "highlight.js/lib/common";
+import Prism from "prismjs";
+
+Prism.manual = true;
 
 const toTree = (str) => {
   const tree = fromMarkdown(str, {
@@ -57,8 +58,6 @@ const mdToHast = (str, schema) => {
   /**
    * 遍历 element 元素:
    * 处理 tagName: raw 的内容, 将 h5 源码转化成 hast
-   *
-   * 处理 tagName: pre > code, 将内容高亮
    */
   visit(hast, "raw", (node) => {
     // 把 html 字符串转化成 hast
@@ -72,9 +71,12 @@ const mdToHast = (str, schema) => {
     delete node.value;
   });
 
+  /**
+   * 遍历 element 元素:
+   * 处理 tagName: pre > code, 将内容高亮
+   */
   visit(hast, "element", (node) => {
     if (node.tagName === "pre") {
-      console.log("处理pre元素", node);
       /**
        * 找出 pre 下面 tagName 是 code 的元素 (注意要找 pre > code, 单纯的 code 元素可能是行内 code)
        * 将 code 元素里的值高亮处理
@@ -88,32 +90,6 @@ const mdToHast = (str, schema) => {
       }
     }
   });
-
-  /**
-   * 处理函数, 处理键值是 type 的对象
-   * 默认只处理一层, 并不会遍历 children
-   */
-  const one = zwitch("type", {
-    handlers: { root, raw: handleRaw, element: handleElement },
-  });
-
-  const handler = (node) => {
-    one(node, handler);
-  };
-
-  function all(nodes, handler) {
-    let index = -1;
-
-    if (nodes) {
-      while (++index < nodes.length) {
-        handler(nodes[index]);
-      }
-    }
-  }
-
-  function root(node, handler) {
-    all(node.children, handler);
-  }
 
   function handleRaw(node) {
     // 把 html 字符串转化成 hast
@@ -128,52 +104,27 @@ const mdToHast = (str, schema) => {
     // console.log("handleRaw", { node, parse5Tree, hast });
   }
 
-  function handleElement(node) {
-    if (node.tagName === "pre") {
-      console.log("处理pre元素", node);
-      /**
-       * 找出 pre 下面 tagName 是 code 的元素 (注意要找 pre > code, 单纯的 code 元素可能是行内 code)
-       * 将 code 元素里的值高亮处理
-       */
-      if (node.children && node.children.length > 0) {
-        node.children.forEach((child) => {
-          if (child.tagName === "code") {
-            handleCode(child);
-          }
-        });
-      }
-    }
-  }
-
   function handleCode(node) {
     if (node.children && node.children.length > 0) {
       const lan = node.properties.lang;
       node.children.forEach((child) => {
-        // todo: 指定语言
         if (lan) {
-          console.log("highlight:", {
-            lan,
-            hljs: hljs.highlight(child.value, { language: lan }),
-          });
-          child.value = hljs.highlight(child.value, { language: lan }).value;
+          const text = Prism.highlight(
+            child.value,
+            Prism.languages[lan] || Prism.languages.txt,
+            lan
+          );
+
+          child.value = text;
         } else {
-          console.log("highlight-auto:", {
-            lan,
-            hljs: hljs.highlight(child.value, { language: lan }),
-          });
-          child.value = hljs.highlightAuto(child.value).value;
+          const text = Prism.highlight(child.value, Prism.languages.txt, "txt");
+
+          child.value = text;
         }
       });
     }
   }
 
-  // function unknown() {
-  //   console.log("unknow do noting");
-  // }
-
-  // one(hast, handler);
-
-  // console.log({ hast });
   return sanitize(hast, schema);
 };
 
